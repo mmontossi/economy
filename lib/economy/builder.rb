b7367ae0c6fb171e3481b5f32c2448a19be47c2a
Mathías Montossi <mmontossi@museways.com>
2018-09-04 13:58:41 -0300

    Add if and unless options

--- lib/economy/builder.rb
+++ lib/economy/builder.rb
@@ -9,6 +9,7 @@ module Economy
     end
 
     def define(*args)
+      options = args.extract_options!
       if args.last.is_a?(Proc)
         renderer = args.pop
       end
@@ -18,10 +19,11 @@ module Economy
         else
           currency_attribute = :currency
         end
+        condition_attribute = "#{attribute}_is_money?"
         set_currency attribute, currency_attribute
-        define_helpers attribute
-        define_getter attribute, currency_attribute, renderer
-        define_setter attribute, currency_attribute
+        define_helpers attribute, condition_attribute, options
+        define_getter attribute, currency_attribute, condition_attribute, renderer
+        define_setter attribute, currency_attribute, condition_attribute
       end
       model.include concern
     end
@@ -32,7 +34,7 @@ module Economy
       model.belongs_to currency_attribute, class_name: 'Economy::Currency'
     end
 
-    def define_helpers(attribute)
+    def define_helpers(attribute, condition_attribute, options)
       concern.class_eval do
         define_method "#{attribute}_came_from_user?" do
           true
@@ -40,43 +42,68 @@ module Economy
         define_method "#{attribute}_before_type_cast" do
           send(attribute).to_json
         end
-      end
-    end
-
-    def define_getter(attribute, currency_attribute, renderer)
-      concern.class_eval do
-        define_method attribute do
-          value = read_attribute(attribute)
-          currency = send(currency_attribute)
-          money = Economy::Money.new(
-            self,
-            (value || 0),
-            currency,
-            renderer
-          )
-          if enforced_currency = Economy.configuration.currency
-            money.exchange_to enforced_currency
+        target = (options[:if] || options[:unless])
+        opposite = (options[:unless] ? true : false)
+        define_method condition_attribute do
+          if target
+            value = begin
+              case target
+              when Symbol
+                send target
+              when Proc
+                instance_exec &target
+              end
+            end
+            opposite ? value.! : value
           else
-            money
+            true
           end
         end
       end
     end
 
-    def define_setter(attribute, currency_attribute)
+    def define_getter(attribute, currency_attribute, condition_attribute, renderer)
       concern.class_eval do
-        define_method "#{attribute}=" do |value|
-          case value
-          when Money
-            if currency_attribute == :currency
-              currency = send(currency_attribute)
-              write_attribute attribute, value.exchange_to(currency).amount
+        define_method attribute do
+          if send(condition_attribute)
+            value = read_attribute(attribute)
+            currency = send(currency_attribute)
+            money = Economy::Money.new(
+              self,
+              (value || 0),
+              currency,
+              renderer
+            )
+            if enforced_currency = Economy.configuration.currency
+              money.exchange_to enforced_currency
             else
-              write_attribute attribute, value.amount
-              write_attribute currency_attribute, value.currency.iso_code
+              money
             end
           else
-            write_attribute attribute, value
+            super()
+          end
+        end
+      end
+    end
+
+    def define_setter(attribute, currency_attribute, condition_attribute)
+      concern.class_eval do
+        define_method "#{attribute}=" do |value|
+          if send(condition_attribute)
+            case value
+            when Money
+              if currency_attribute == :currency
+                currency = send(currency_attribute)
+                write_attribute attribute, value.exchange_to(currency).amount
+              else
+                write_attribute attribute, value.amount
+                write_attribute currency_attribute, value.currency.iso_code
+              end
+            else
+              write_attribute attribute, value
+            end
+          else
+            super value
           end
         end
       end
